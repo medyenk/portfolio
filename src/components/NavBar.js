c743c7a4eca6d965aae51b5611732433f96b1e32
Medyen <medianak@hotmail.co.uk>
2021-01-27 16:37:26 +0000

    Deleted unused files

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                {/* <Link>Home</Link>
+                <Link>Projects</Link>
+                <Link>Skills</Link>
+                <Link>Contact</Link> */}
+                Home | Projects | Contact | About
+            </div>
+        )
+    }
+}
